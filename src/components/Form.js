e72b60596f2b90c3e64d025838544f69190cad05
swk3169 <swk3169@gmail.com>
2018-07-25 18:20:45 +0900

    update-ver0.01
    TodoItem 추가

---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+// file: src/components/Form.js
+import React from 'react';
+import './Form.css';
+
+const Form = ({value, onChange, onCreate, onKeyPress}) => {
+    return (
+        <div className="form">
+            <input value={value} onChange={onChange} onKeyPress={onKeyPress}/>
+            <div className="create-button" onClick={onCreate}>
+                추가
+            </div>
+        </div>
+    );
+};
+
+export default Form;
